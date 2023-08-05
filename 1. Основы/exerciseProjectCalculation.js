5a07d40b08d459bdf7ab7f7638db7d56d5af84d5
Tema Tompsen <not@notbadone.ru>
2023-08-05 18:07:08 +0300

    Простая задача на понимание прощедшего

--- 1. Основы/exerciseProjectCalculation.js
+++ 1. Основы/exerciseProjectCalculation.js
@@ -1,3 +1,7 @@
 const payHour = 80;
 const hourOnDay = 5;
-const dayInWeek = 5;
+const dayOnLeave = (11 - 2) * hourOnDay;
+const project = 40;
+
+console.log('Смогу ли взять в работу проект: ' + (project < dayOnLeave));
+console.log('Возьму за проект: ' + (project * payHour) + ' долларов');
