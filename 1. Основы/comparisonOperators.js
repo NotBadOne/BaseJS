d5d4b56f19a091e330ac2145a43c163326fef515
Tema Tompsen <not@notbadone.ru>
2023-08-05 17:10:24 +0300

    Операторы сравнения

--- 1. Основы/comparisonOperators.js
+++ 1. Основы/comparisonOperators.js
@@ -3,15 +3,8 @@
 let age = 22;
 const vasya = 20;
 
-<<<<<<< HEAD
-
-console.log(age > vasya);
-console.log(age < vasya);
-console.log(age >= vasya);
-=======
 console.log(age > vasya);
 console.log(age >= vasya);
 console.log(age < vasya);
->>>>>>> e52c849 (Порядок операторов)
 console.log(age <= vasya);
 console.log(age == vasya);
