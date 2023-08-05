8bfb30ffb5e6149555cb7e79c561b3d408c905fe
Tema Tompsen <not@notbadone.ru>
2023-08-05 17:08:06 +0300

    Порядок операторов

--- 1. Основы/orderOperators.js
+++ 1. Основы/orderOperators.js
@@ -1,5 +1,18 @@
 // Порядок операторов
+
 // Например: const isSuited = box - 10 > size - 5;
+
+// Таблица приоритетов операторов
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
 
 const isSuited = 100 - 10 > 90 - 5;
 console.log(isSuited);
+
+// 100 - 10 (12)
+// 90 - 5 (12)
+// Потом >
+
+let a = 6 + 10 / 2;
+console.log(a);
+a = (6 + 10) / 2;
+console.log(a);
