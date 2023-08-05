8a5ff0b3506a0bd30116a2af9f47e994776a4584
Tema Tompsen <not@notbadone.ru>
2023-08-05 16:46:35 +0300

    Базовые рифметические операции

---
+++ 1. Основы/arithmeticOperations.js
@@ -0,0 +1,20 @@
+// Базовые рифметические операции
+
+// Числовые
+const width = 10;
+const height = 5;
+const space = 10 * 5;
+const widthNew = width - 4;
+const devision = space / widthNew;
+const volume = 2 ** 3; // 2 * 2 * 2
+
+console.log(space);
+console.log(widthNew);
+console.log(devision);
+console.log(volume);
+
+// Строковые
+const city = 'Ярославль';
+const street = 'Большая октябрьская';
+
+console.log(city + ', ' + street + ' ' + 37);
